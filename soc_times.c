f4917b71bf565466074e8db84598e35c46e5527c
Prudhvi Krishna Surapaneni <prudhvis@investigativenetworks.com>
2010-09-23 12:08:14 -0500

    Remote un-wanted headers

--- soc_times.c
+++ soc_times.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <time.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <string.h>
 #include <netdb.h>
 #include <ctype.h>
